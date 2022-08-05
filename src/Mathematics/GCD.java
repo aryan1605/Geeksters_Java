850219bdfb5504e213ac88a81c3db1dcbeb330ce
Aryan Gupta <aryangupta1605@gmail.com>
2022-08-05 19:05:37 +0530

    Calculating the GCD of two numbers 'A' and 'B'.

---
+++ src/Mathematics/GCD.java
@@ -0,0 +1,21 @@
+package Mathematics;
+import java.util.Scanner;
+
+public class GCD {
+    public static int gcd(int A,int B)
+    {  //        Assumption : A % B
+        if(B == 0)
+        {
+            return A;
+        }
+        return gcd(B,A % B);
+        // Time Complexity = O(log n)
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two numbers :");
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println("GCD of two numbers = "+gcd(A,B));
+    }
+}
