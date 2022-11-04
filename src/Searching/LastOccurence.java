c8affbbe165173190dbe61e978a28e5fe7a58ae4
Aryan Gupta <aryangupta1605@gmail.com>
2022-11-04 22:40:58 +0530

    Last Occurence of the element in a sorted array.

---
+++ src/Searching/LastOccurence.java
@@ -0,0 +1,46 @@
+package Searching;
+import java.util.Scanner;
+
+public class LastOccurence {
+    public static int Last_Occurence(int [] arr,int low,int end,int x)
+    {
+        while(low <= end)
+        {
+            int mid = low + (end - low)/2;
+            if(arr[mid] < x)
+            {
+                low = mid+1;
+            }
+            else if(arr[mid] > x)
+                end = mid - 1;
+            else
+            {
+                if(mid == arr.length-1 || arr[mid] != arr[mid+1])
+                    return mid;
+                else
+                    low = mid + 1;
+            }
+        }
+        return -1;
+        // Time Complexity = O(log n)
+        // Auxiliary Space = O(1)
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        System.out.println("Enter the elements : ");
+        for(int i = 0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the element to be found : ");
+        int x = sc.nextInt();
+        int pos = Last_Occurence(arr,0,n-1,x);
+        if(pos != -1)
+            System.out.println("Last Occurence of element is found at position "+(pos+1));
+        else
+            System.out.print("Element Not Found");
+    }
+}
